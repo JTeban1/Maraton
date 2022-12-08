58711f98c932fc05949fc04c94917e626a407624
xxSTIVENxx <stivencode.08@gmail.com>
2022-12-07 20:07:29 -0500

    Ajustes formularios

--- views/pages/personal/actualizarpersonal.php
+++ views/pages/personal/actualizarpersonal.php
@@ -1,3 +1,21 @@
+<?php
+
+if(isset($_GET["id"])){
+
+	$item = "id";
+	$valor = $_GET["id"];
+
+
+     $personal = ControladorPersonal ::ctrSeleccionarRegistroPersonal($item, $valor);
+     $rol = ControladorRol::ctrSeleccionarRegistroRol($item, $valor);
+     $posicion = ControladorPosicion::ctrSeleccionarRegistroPosi($item, $valor);
+     
+
+}
+
+?>
+
+
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
